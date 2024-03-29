ac17570a4f25e19d23c6560cc9b89adeb0c6058a
Tarun Khandelwal <divya@Taruns-MacBook-Pro.local>
2024-03-30 00:50:07 +0530

    fix css

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,8 @@ const App = () => {
         <Header />
 
         <select
-          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 w-32 float-right mr-4'
+          style={{ width: 200 }}
+          className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 float-right mr-4'
           value={cluster}
           onChange={event => {
             setCluster(event.target.value as any);
